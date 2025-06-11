aebab570b41c1d34b5ca8fa5d44785187bcb6faf
Dashiell <95331081+Dashiell1234@users.noreply.github.com>
2025-06-11 13:53:35 +1200

    Delete interactive-comments-section-main directory

--- interactive-comments-section-main/index.js
+++
@@ -1,3 +0,0 @@
-function openWin() {
-  window.open("file:///C:/Users/d.m.mcmillan/OneDrive%20-%20Saint%20Kentigern/Y13/COM/HTML/interactive-comments-section-main/interactive-comments-section-main/coment.html");
-}
